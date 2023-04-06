aff2500277236f9b0a6dcb69a76fb9ecadc1ac68
Mehdi Raheemun <mraheemu@student.42.fr>
2023-04-06 03:27:07 +0200

    Routage Home et APropos

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home/Home.jsx";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+function NotFoundPage() {
+  return <h1>Page non trouvé</h1>;
+}
+
+export default NotFoundPage;
