190bbab912a670bea11262cbc415ffd510c2e2a2
Mehdi Raheemun <mraheemu@student.42.fr>
2023-04-16 02:18:07 +0200

    création complete NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,22 @@
+import styles from "./NotFoundPage.module.css";
+import Header from "../../components/Header/Header";
+import { NavLink } from "react-router-dom";
+
 function NotFoundPage() {
-  return <h1>Page non trouvé</h1>;
+  return (
+    <>
+      <Header />
+      <div className={styles["nfp-container"]}>
+        <div>
+          <h1>404</h1>
+          <p>{"Oups! La page que vous demandez n'existe pas."}</p>
+        </div>
+        <NavLink to="/">
+          <p>{"Retourner sur la page d'accueil"}</p>
+        </NavLink>
+      </div>
+    </>
+  );
 }
 
 export default NotFoundPage;
