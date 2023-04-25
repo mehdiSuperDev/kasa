1dc14eab58e25eef9688802761c62db72018dde1
Mehdi Raheemun <mraheemu@student.42.fr>
2023-04-25 18:07:05 +0200

    fix collapse largeur et debut MEP cardDetails

--- src/pages/CardDetails/CardDetails.jsx
+++ src/pages/CardDetails/CardDetails.jsx
@@ -2,6 +2,8 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./CardDetails.module.css";
 import { useParams } from "react-router-dom";
+import styles from "./CardDetails.module.css";
+import Collapse from "../../components/Collapse/Collapse";
 
 function CardDetails() {
   const { id } = useParams();
@@ -9,7 +11,12 @@ function CardDetails() {
   return (
     <>
       <Header />
+      <div className={styles["header"]}></div>
       <h1>{`CardDetails Page with id: ${id}`}</h1>
+      <div className={styles["body-footer"]}>
+        <Collapse title="title" body="body" />
+        <Collapse title="title" body="body" />
+      </div>
       <Footer />
     </>
   );
